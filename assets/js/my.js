cab6564a21291058052a2fcedcd6a866fb774a34
Maw Paing Thu <mawpaingthu@comradesol.com>
2020-07-21 15:28:48 +0900

    version 0.1

--- assets/js/my.js
+++ assets/js/my.js
@@ -1,147 +1,81 @@
-
-    // var app = new Vue({
-    //   el: '#app1',
-    //   data: {
-    //     title: "App1",
-    //     message: "Hello Vue. I'm comming."
-    //   }
-    // })
-
-    // var app2 = new Vue({
-    //   el: '#app2',
-    //   data:{
-    //     title: "App2",
-    //     message: 'you loaded this page on ' + new Date().toLocaleString()
-    //   }
-    // })
-
-    // var app3 = new Vue({
-    //   el: '#app3',
-    //   data: {
-    //     title: "App3",
-    //     seen: true
-    //   }
-    // })
-
-    // var app4 = new Vue({
-    //   el: '#app4',
-    //   data:{
-    //     title: "App4",
-    //     todos:[
-    //       {text: 'Learn Javascript'},
-    //       {text: 'Learn Vue'},
-    //       {text: 'Build something awesome'}
-    //     ]
-    //   }
-    // })
-
-    // var app5 = new Vue({
-    //   el: '#app5',
-    //   data:{
-    //     title: "App5",
-    //     message: "Hello Vue.js!"
-    //   },
-    //   methods:{
-    //     reverseMessage: function(){
-    //       this.message = this.message.split('').reverse().join('');
-    //     }
-    //   }
-    // })
-
-    // var app6 = new Vue({
-    //     el:'#app6',
-    //     data:{
-    //         title: "App6",
-    //         message: ""
-    //     }
-    // })
-
-
-    // Vue.component('todo-item',{
-    //     props:['todo'],
-    //     template: '<li>{{todo.text}}</li>'
-    // })
-    // var app7 = new Vue({
-    //     el:'#app7',
-    //     data: {
-    //         title: "App7",
-    //         groceryList:[
-    //             {id:0, text:'Vegetables'},
-    //             {id:1, text:'Cheese'},
-    //             {id:2, text:'Whatever else humans are supposed to eat'}
-    //         ]
-    //     }
-    // })
-
     var tite = "<h1>{{title}}</h1>";
     var con = "<span>{{welMsg}}</span>";
-    var kagyi = "<div class='mm_alphabert'><span v-on:click = 'showKagyi'>က</span>";
-    var kakhway = "<span v-on:click = 'showKakway'>ခ</span>";
-    var gange = "<span v-on:click = 'showGange'>ဂ</span>";
-    var gagyi = "<span v-on:click = 'showGagyi'>ဃ</span>";
-    var nga = "<span v-on:click = 'showNga'>င</span></div>";
-    var phrase = "<div class='showPhrase'><p :class='className'>{{message}}</p></div>";
-    
+
     var navList = [
                     {nav: 'Home'},
                     {nav: 'Service'},
                     {nav: 'About'},
                     {nav: 'Contact Us'}
                   ];
-    var classList = [
-                      {class: 'Basic',fees:'xxx,xxx',startDate:'2020/Aug'},
-                      {class: 'Intermediate',fees:'xxx,xxx',startDate:'2020/Aug'},
-                      {class: 'Advanced',fees:'xxx,xxx',startDate:'2020/Aug'},
+    var wordList = [
+                      {word: 'က',phrase:'က - ကလေးငယ် ချစ်စဖွယ်',color:'yellow'},
+                      {word: 'ခ',phrase:'ခ - ခရေကုံး မလေးပြုံ',color:'green'},
+                      {word: 'ဂ',phrase:'ဂ - ဂဏန်းသင် ပြုံးရွှင်ရွှင်',color:'white'},
+                      {word: 'ဃ',phrase:'ဃ - ',color: 'red'},
+                      {word: 'င',phrase:'င - ငချိတ်ပေါင်း စားလို့ကောင်း',color:'blue'},
+                      {word: 'စ',phrase:'စ - စခန်းသာ တောင်တန်းပြာ',color:'yellow'},
+                      {word: 'ဆ',phrase:'ဆ - ဆရာမ ရိုသေကြ',color:'green'},
+                      {word: 'ဇ',phrase:'ဇ - ဇလပ်ဖြူသင်းကြူကြူ',color:'white'},
+                      {word: 'ဈ',phrase:'ဈ - ',color: 'red'},
+                      {word: 'ည',phrase:'ည - ညနေခင်း ရေခပ်ဆင်း',color:'blue'},
+                      {word: 'ဋ',phrase:'ဋ - ',color:'yellow'},
+                      {word: 'ဌ',phrase:'ဌ - ',color:'green'},
+                      {word: 'ဍ',phrase:'ဍ - ',color:'white'},
+                      {word: 'ဎ',phrase:'ဎ - ',color: 'red'},
+                      {word: 'ဏ',phrase:'ည - ညနေခင်း ရေခပ်ဆင်း',color:'blue'},
+                      {word: 'တ',phrase:'တ - တမာပင် ရွာအဝင်',color:'yellow'},
+                      {word: 'ထ',phrase:'ထ - ထမင်းပွဲ လက်ဆုံနွှဲ',color:'green'},
+                      {word: 'ဒ',phrase:'ဒ - ဒရယ်ရုံ သားမျိုးစုံ',color:'white'},
+                      {word: 'ဓ',phrase:'ဓ - ဓနိတော',color: 'red'},
+                      {word: 'န',phrase:'န - ',color:'blue'},
+                      {word: 'ပ',phrase:'ပ - ',color:'yellow'},
+                      {word: 'ဖ',phrase:'ဖ - ဖရုံယို အလွန်ချို',color:'green'},
+                      {word: 'ဗ',phrase:'ဗ - ဗလကြီး ဆင်ပြောင်စီး',color:'white'},
+                      {word: 'ဘ',phrase:'ဘ - ',color: 'red'},
+                      {word: 'မ',phrase:'မ - မမလေး ဝလုံးရေး',color:'blue'},
+                      {word: 'ယ',phrase:'ယ - ယမင်းခင် ကကြိုးဆင်',color:'yellow'},
+                      {word: 'ရ',phrase:'ရ - ရထားကြီး ပျော်ပျော်စီး',color:'green'},
+                      {word: 'လ',phrase:'လ - လဝါဝါ ထိန်ထိန်သာ',color:'white'},
+                      {word: 'ဝ',phrase:'ဝ - ',color: 'red'},
+                      {word: 'သ',phrase:'သ - ',color:'blue'},
+                      {word: '-',phrase:'',color:'yellow'},
+                      {word: 'ဟ',phrase:'ဟ - ',color:'green'},
+                      {word: 'ဠ',phrase:'ဠ - ',color:'white'},
+                      {word: 'အ',phrase:'အ - ',color: 'red'},
+                      {word: '-',phrase:'',color:'blue'},
                     ]
     Vue.component('app-nav',{
       props:['toshow'],
       template:'<a>{{toshow.nav}}</a>'
     })
-    
+
     Vue.component('app-body',{
-      template: "<div class='body'>" + tite + ""  + con + "" + kagyi+ "" + kakhway + "" + gange + "" + gagyi + "" + nga + "" + phrase +"</div>",
+      template: "<div class='body'>" + con + ""  + tite + "</div>",
       data:function(){
         return{
-          title:'Ur Online Education',
-          welMsg:'Welcome to our O-EDU!',
-          message: "",
-          className:"origin"
+          title:'မြန်မာစာ ဗျည်းအက္ခရာ (၃၃) လုံး',
+          welMsg:'မြန်မာစာ သင်ပုန်းကြီး',
         }
       },
-      methods:{
-        showKagyi:function(){
-          this.message = "က - ကလေးငယ် ချစ်စဖွယ်";
-          this.className = "yellow";
-        },
-        showKakway:function(){
-          this.message = "ခ - ခရေကုံး မလေးပြုံး";
-          this.className = "green";
-        },
-        showGange:function(){
-          this.message = "ဂ - ဂဏန်းသင် ပြုံးရွှင်ရွှင်";
-          this.className = "white";
-        },
-        showGagyi:function(){
-          this.message = "ဃ - ";
-          this.className = "red";
-        },
-        showNga:function(){
-          this.message = "င - ငချိတ်ပေါင်း စားလို့ကောင်း";
-          this.className = "blue";
-        },
-      }
     })
     Vue.component('app-content',{
-      props:['conte'],
-      template: "<div class='content'><h3>{{conte.class}}</h3><h4>{{conte.fees}}</h4><h4>{{conte.startDate}}</h4></div>"
+      props:['todo'],
+      template: "<span :class = 'todo.color' v-on:click = 'showPhrase(todo.phrase,todo.color)'>{{todo.word}}</span>",
+      methods:{
+        showPhrase:function(phrase,color){
+          app8.message = phrase;
+          app8.className = color;
+        },
+      }
     })
     var app8 = new Vue({
       el: '#app8',
       data:{
         title: "App8",
         navList: navList,
-        contentList: classList,        
+        wordList: wordList,
+        message: "က - ကလေးငယ် ချစ်စဖွယ်",
+        className:"yellow"
       },
-      
-    })
 
+    })
